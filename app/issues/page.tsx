077691d42df38bca43da9a28337dac070dcbfcbf
Pau Vila <pvila89@gmail.com>
2023-10-18 10:01:21 +0200

    Show the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,14 +2,50 @@ import React from 'react'
 
 import Link from 'next/link'
 
-import { Button } from '@radix-ui/themes'
+import prisma from '@/prisma/client'
+import { Button, Table, TableColumnHeaderCell } from '@radix-ui/themes'
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+    const issues = await prisma.issue.findMany()
+
     return (
         <div>
-            <Button>
-                <Link href="/issues/new">New Issue</Link>
-            </Button>
+            <div className="mb-5">
+                <Button>
+                    <Link href="/issues/new">New Issue</Link>
+                </Button>
+            </div>
+            <Table.Root variant="surface">
+                <Table.Header>
+                    <Table.Row>
+                        <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
+                        <TableColumnHeaderCell className="hidden md:table-cell">
+                            Status
+                        </TableColumnHeaderCell>
+                        <TableColumnHeaderCell className="hidden md:table-cell">
+                            Created
+                        </TableColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {issues.map((issue) => (
+                        <Table.Row key={issue.id}>
+                            <Table.Cell>
+                                {issue.title}
+                                <div className="block md:hidden">
+                                    {issue.status}
+                                </div>
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                {issue.status}
+                            </Table.Cell>
+                            <Table.Cell className="hidden md:table-cell">
+                                {issue.createdAt.toDateString()}
+                            </Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table.Root>
         </div>
     )
 }
