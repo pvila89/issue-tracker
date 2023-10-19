073fa46007bc40530c4d608468f84c45dc83384a
Pau Vila <pvila89@gmail.com>
2023-10-19 11:49:46 +0200

    Refactor: Apply the SRP

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Link from 'next/link';
+
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { Button } from '@radix-ui/themes';
+
+interface Props {
+    issueId: number
+}
+
+const EditIssueButton = ({issueId}: Props) => {
+    return (
+        <Button>
+            <Pencil2Icon />
+            <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
+        </Button>
+    )
+}
+
+export default EditIssueButton
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,21 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-import Link from 'next/link';
-import { notFound } from 'next/navigation';
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import { notFound } from 'next/navigation'
 
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import prisma from '@/prisma/client';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import {
-  Box,
-  Button,
-  Card,
-  Flex,
-  Grid,
-  Heading,
-  Text,
-} from '@radix-ui/themes';
+import prisma from '@/prisma/client'
+import { Box, Grid } from '@radix-ui/themes'
+
+import EditIssueButton from './EditIssueButton'
+import IssueDetails from './IssueDetails'
 
 interface Props {
     params: {
@@ -35,20 +26,10 @@ const IssueDetailPage = async ({ params }: Props) => {
     return (
         <Grid columns={{ initial: '1', md: '2' }} gap="5">
             <Box>
-                <Heading>{issue.title}</Heading>
-                <Flex gap="3" my="2">
-                    <IssueStatusBadge status={issue.status} />
-                    <Text>{issue.createdAt.toDateString()}</Text>
-                </Flex>
-                <Card className="prose" mt="4">
-                    <ReactMarkdown>{issue.description}</ReactMarkdown>
-                </Card>
+                <IssueDetails issue={issue} />
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon />
-                    <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-                </Button>
+                <EditIssueButton issueId={issue.id} />
             </Box>
         </Grid>
     )
