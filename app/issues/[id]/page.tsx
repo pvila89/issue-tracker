53a25ac8d31302e38aa508ef2d80414428685382
Pau Vila <pvila89@gmail.com>
2023-10-19 22:51:15 +0200

    Build the assigne select component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-import { getServerSession } from 'next-auth'
-import { notFound } from 'next/navigation'
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
 
-import authOptions from '@/app/auth/authOptions'
-import prisma from '@/prisma/client'
-import { Box, Flex, Grid } from '@radix-ui/themes'
+import authOptions from '@/app/auth/authOptions';
+import prisma from '@/prisma/client';
+import {
+  Box,
+  Flex,
+  Grid,
+} from '@radix-ui/themes';
 
-import DeleteIssueButton from './DeleteIssueButton'
-import EditIssueButton from './EditIssueButton'
-import IssueDetails from './IssueDetails'
+import AssigneeSelect from './AssigneeSelect';
+import DeleteIssueButton from './DeleteIssueButton';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 
 interface Props {
     params: {
@@ -36,6 +41,7 @@ const IssueDetailPage = async ({ params }: Props) => {
             {session && (
                 <Box>
                     <Flex direction="column" gap="4">
+                        <AssigneeSelect />
                         <EditIssueButton issueId={issue.id} />
                         <DeleteIssueButton issueId={issue.id} />
                     </Flex>
