dcf4a600eed101b92f5fb39b57738eb27e7e6446
Pau Vila <pvila89@gmail.com>
2023-10-20 10:03:26 +0200

    Set up React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
-import '@radix-ui/themes/styles.css'
-import './theme-config.css'
-import './globals.css'
+import '@radix-ui/themes/styles.css';
+import './theme-config.css';
+import './globals.css';
 
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-import AuthProvider from '@/app/auth/Provider'
-import { Container, Theme } from '@radix-ui/themes'
+import AuthProvider from '@/app/auth/Provider';
+import {
+  Container,
+  Theme,
+} from '@radix-ui/themes';
 
-import { NavBar } from './NavBar'
+import { NavBar } from './NavBar';
+import QueryClientProvider from './QueryClientProvider';
 
 const inter = Inter({
     subsets: ['latin'],
@@ -26,16 +30,18 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <AuthProvider>
-                <body className={inter.className}>
-                    <Theme accentColor="violet">
-                        <NavBar />
-                        <main className="p-5">
-                            <Container>{children}</Container>
-                        </main>
-                    </Theme>
-                </body>
-            </AuthProvider>
+            <QueryClientProvider>
+                <AuthProvider>
+                    <body className={inter.className}>
+                        <Theme accentColor="violet">
+                            <NavBar />
+                            <main className="p-5">
+                                <Container>{children}</Container>
+                            </main>
+                        </Theme>
+                    </body>
+                </AuthProvider>
+            </QueryClientProvider>
         </html>
     )
 }
