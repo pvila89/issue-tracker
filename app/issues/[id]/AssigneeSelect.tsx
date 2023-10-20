dbac5def7168f7658b7391121ae6bbe5d9d98315
Pau Vila <pvila89@gmail.com>
2023-10-20 11:14:52 +0200

    Assign an issue to an user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -5,11 +5,11 @@ import React from 'react'
 import axios from 'axios'
 import Skeleton from 'react-loading-skeleton'
 
-import { User } from '@prisma/client'
+import { Issue, User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     const {
         data: users,
         error,
@@ -26,11 +26,19 @@ const AssigneeSelect = () => {
     if (error) return null
 
     return (
-        <Select.Root>
+        <Select.Root
+            defaultValue={issue.assignedToUserId || 'unassigned'}
+            onValueChange={(userId) => {
+                axios.patch('/api/issues/' + issue.id, {
+                    assignedToUserId: userId === 'unassigned' ? null : userId,
+                })
+            }}
+        >
             <Select.Trigger placeholder="Assign..." />
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
+                    <Select.Item value="unassigned">Unassigned</Select.Item>
                     {users?.map((user) => (
                         <Select.Item key={user.id} value={user.id}>
                             {user.name}
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,20 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-import { getServerSession } from 'next-auth';
-import { notFound } from 'next/navigation';
+import { getServerSession } from 'next-auth'
+import { notFound } from 'next/navigation'
 
-import authOptions from '@/app/auth/authOptions';
-import prisma from '@/prisma/client';
-import {
-  Box,
-  Flex,
-  Grid,
-} from '@radix-ui/themes';
+import authOptions from '@/app/auth/authOptions'
+import prisma from '@/prisma/client'
+import { Box, Flex, Grid } from '@radix-ui/themes'
 
-import AssigneeSelect from './AssigneeSelect';
-import DeleteIssueButton from './DeleteIssueButton';
-import EditIssueButton from './EditIssueButton';
-import IssueDetails from './IssueDetails';
+import AssigneeSelect from './AssigneeSelect'
+import DeleteIssueButton from './DeleteIssueButton'
+import EditIssueButton from './EditIssueButton'
+import IssueDetails from './IssueDetails'
 
 interface Props {
     params: {
@@ -41,7 +37,7 @@ const IssueDetailPage = async ({ params }: Props) => {
             {session && (
                 <Box>
                     <Flex direction="column" gap="4">
-                        <AssigneeSelect />
+                        <AssigneeSelect issue={issue} />
                         <EditIssueButton issueId={issue.id} />
                         <DeleteIssueButton issueId={issue.id} />
                     </Flex>
