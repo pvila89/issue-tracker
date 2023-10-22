7cf5072c0a64e6960045914be17414bfc960896b
Pau Vila <pvila89@gmail.com>
2023-10-22 12:52:46 +0200

    Build the layout of the paginatino component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Pagination from './components/Pagination'
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
-  )
+    return (
+        <main>
+            <Pagination itemCount={100} pageSize={10} currentPage={10} />
+        </main>
+    )
 }
